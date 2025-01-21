4d52bb9e3f49d37953bf55d881c4dae032de9281
valeria <vebriferr@gmail.com>
2025-01-21 07:58:19 +0100

    g

--- Reto1ev2/src/ejercicios/FuncionMenu.java
+++ Reto1ev2/src/ejercicios/FuncionMenu.java
@@ -1,15 +1,19 @@
 package ejercicios;
 
+import clases.Cliente;
+
 public class FuncionMenu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-//funcion Mostrar compras: escribirá las compras realizadas por tienda, mostrando el nombre de la tienda y el total de productos comprados en ella. 
+//funcion Mostrar compras: escribirá las compras realizadas por tienda, 
+//mostrando el nombre de la tienda y el total de productos comprados en ella. 
 //Al final mostrará el gasto total.
 
-public static void mostrarCompras() {
-	 
+public static void mostrarCompras(Cliente cliente) {
+	
+	
 }
 }
